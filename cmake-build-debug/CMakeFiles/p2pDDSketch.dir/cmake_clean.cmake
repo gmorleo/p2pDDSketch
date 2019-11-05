40b57868fb02ced873d73d0799216c5add1c00d3
giuseppe <kelt.m91@gmail.com>
2019-11-05 19:46:32 +0100

    First Commit

---
+++ cmake-build-debug/CMakeFiles/p2pDDSketch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p2pDDSketch.dir/main.cpp.o"
+  "p2pDDSketch"
+  "p2pDDSketch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p2pDDSketch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,413 @@
+#include <iostream>
+#include <igraph/igraph.h>
+#include <cstring>
+#include <random>
+#include <map>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+const int DEFAULT_OFFSET = 1073741824; //2^31/2
+const int DEFAULT_BIN_LIMIT = 500;
+const float DEFAULT_ALPHA = 0.01;
+
+void StartTheClock();
+double StopTheClock();
+void usage(char* cmd);
+igraph_t generateGeometricGraph(igraph_integer_t n, igraph_real_t radius);
+igraph_t generateBarabasiAlbertGraph(igraph_integer_t n, igraph_real_t power, igraph_integer_t m, igraph_real_t A);
+igraph_t generateErdosRenyiGraph(igraph_integer_t n, igraph_erdos_renyi_t type, igraph_real_t param);
+igraph_t generateRegularGraph(igraph_integer_t n, igraph_integer_t k);
+igraph_t generateRandomGraph(int type, int n);
+void printGraphType(int type);
+
+struct Params {
+    uint32_t    domainSize;     // limite
+    long        ni;            // points number of dataset
+    int         peers;          // numero peer
+    int         p_star;
+    string      outputFilename;     // Se voglio salvere tutto su file
+    double      convThreshold;      //
+    int         convLimit;          // fissato a 3, quando un peer arriva a convergenza controlla il mio valore attuale con il precedente per almeno 3 iterazioni
+    int         graphType;      //tipo grafo
+    int         fanOut;             // numero massimo vicini con cui comunico
+    int         roundsToExecute;       // round da eseguire senza aspettare la convergenza
+    double      delta;
+
+    int         offset;
+    float       alpha;
+    float       gamma;
+    float       ln_gamma;
+    int         bin_limit;
+};
+
+high_resolution_clock::time_point t1, t2;
+
+int main(int argc, char **argv) {
+    /*** Default Parameters***/
+    long        ni;                         // points number
+    long        *peerLastItem;              // index of a peer last item
+    uint32_t    domainSize = 1048575;       // number of possible distinct items
+    int         peers = 10;                 // number of peers
+    int         fanOut = 5;                 // fan-out of peers
+    int         graphType = 2;              // graph distribution: 1 geometric 2 Barabasi-Albert 3 Erdos-Renyi 4 regular (clique)
+    double      convThreshold = 0.0001;     // local convergence tolerance
+    int         convLimit = 3;              // number of consecutive rounds in which a peer must locally converge
+    int         roundsToExecute = -1;       // Se metto un numero esegue quello
+    int         p_star = -1;
+    double      delta = 0.04;
+
+    int         offset = DEFAULT_OFFSET;
+    float       alpha = DEFAULT_ALPHA;
+    float       gamma = (1 + alpha)/(1-alpha);
+    float       ln_gamma = log(gamma);
+    int         bin_limit = DEFAULT_BIN_LIMIT;
+
+
+    double          elapsed;
+    int             iterations;
+    bool            autoseed = false;
+    bool            outputOnFile = false;
+    string          outputFilename;
+    igraph_t        graph;
+    Params          params;
+
+    /*** parse command-line parameters ***/
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-di") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing domain size parameter." << endl;
+                return -1;
+            }
+            domainSize = stol(argv[i]);
+        } else if (strcmp(argv[i], "-delta") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing delta parameter." << endl;
+                return -1;
+            }
+            delta = stod(argv[i]);
+        } else if (strcmp(argv[i], "-p") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing number of peers parameter." << endl;
+                return -1;
+            }
+            peers = stoi(argv[i]);
+        } else if (strcmp(argv[i], "-ps") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing maximum number of peers parameter." << endl;
+                return -1;
+            }
+            p_star = stoi(argv[i]);
+        } else if (strcmp(argv[i], "-f") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing fan-out parameter." << endl;
+                return -1;
+            }
+            fanOut = stoi(argv[i]);;
+        } else if (strcmp(argv[i], "-d") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing graph type parameter" << endl;
+                return -1;
+            }
+            graphType = stoi(argv[i]);
+        } else if (strcmp(argv[i], "-ct") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing convergence tolerance parameter." << endl;
+                return -1;
+            }
+            convThreshold = stod(argv[i]);
+        } else if (strcmp(argv[i], "-cl") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing # of consecutive rounds in which convergence is satisfied parameter." << endl;
+                return -1;
+            }
+            convLimit = stol(argv[i]);
+        } else if (strcmp(argv[i], "-of") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing filename for simulation output." << endl;
+                return -1;
+            }
+            outputFilename = string(argv[i]);
+        } else if (strcmp(argv[i], "-r") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing number of rounds to execute.\n";
+                return -1;
+            }
+            roundsToExecute = stoi(argv[i]);
+        } else if (strcmp(argv[i], "-al") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing number of precision for raster.\n";
+                return -1;
+            }
+            alpha = stod(argv[i]);
+        } else if (strcmp(argv[i], "-ofs") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing number of threshold for raster.\n";
+                return -1;
+            }
+            offset = stoi(argv[i]);
+        } else if (strcmp(argv[i], "-bl") == 0) {
+            i++;
+            if (i >= argc) {
+                cerr << "Missing number of min size for raster.\n";
+                return -1;
+            }
+            bin_limit = stoi(argv[i]);
+        }else if (strcmp(argv[i], "-as") == 0) {
+            autoseed = true;
+        } else {
+            //usage(argv[0]);
+            return -1;
+        }
+    }
+
+    /*** assign parameters read from command line ***/
+    params.ni = ni;
+    params.domainSize = domainSize;
+    params.peers = peers;
+    params.fanOut = fanOut;
+    params.graphType = graphType;
+    params.convThreshold = convThreshold;
+    params.convLimit = convLimit;
+    params.outputFilename = outputFilename;
+    params.roundsToExecute = roundsToExecute;
+    params.delta = delta;
+    // Parameters of the algorithm
+    params.alpha = alpha;
+    params.gamma = gamma;
+    params.ln_gamma = ln_gamma;
+    params.offset = offset;
+    params.bin_limit = bin_limit;
+
+    outputOnFile = !params.outputFilename.empty();
+    if (!outputOnFile) {
+        printf("\n\nPARAMETERS:\n");
+        //printf("distinct items in the stream = %d\n", params.domainSize);
+        //cout << "dataset = " << params.name_file << "\n";
+        //cout << "n° points = " << params.ni << "\n";
+        cout << "alpha = " << params.alpha << "\n";
+        cout << "gamma = " << params.gamma << "\n";
+        cout << "offset = " << params.offset << "\n";
+        cout << "bin_limit = " << params.bin_limit << "\n";
+        cout << "peers = " << params.peers << "\n";
+        cout << "fan-out = " << params.fanOut << "\n";
+        cout << "graph type = ";
+        printGraphType(params.graphType);
+        cout << "local convergence tolerance = "<< params.convThreshold << "\n";
+        cout << "number of consecutive rounds in which a peer must locally converge = "<< params.convLimit << "\n";
+        cout << "\n\n";
+    }
+
+    /*** Graph generation ***/
+    // turn on attribute handling in igraph
+    igraph_i_set_attribute_table(&igraph_cattribute_table);
+
+    // seed igraph PRNG
+    igraph_rng_seed(igraph_rng_default(), 42);
+
+    StartTheClock();
+    // generate a connected random graph
+    graph = generateRandomGraph(params.graphType, params.peers);
+
+    double gengraphtime = StopTheClock();
+    if (!outputOnFile) {
+        cout <<"Time (seconds) required to generate the random graph: " << gengraphtime << "\n";
+    }
+
+    // determine minimum and maximum vertex degree for the graph
+    igraph_vector_t result;
+    igraph_real_t mindeg;
+    igraph_real_t maxdeg;
+
+    igraph_vector_init(&result, 0);
+    igraph_degree(&graph, &result, igraph_vss_all(), IGRAPH_ALL, IGRAPH_NO_LOOPS);
+    igraph_vector_minmax(&result, &mindeg, &maxdeg);
+    if (!outputOnFile) {
+        cout << "Minimum degree is "  <<  mindeg << ", Maximum degree is " << maxdeg << "\n";
+    }
+
+
+    return 0;
+}
+
+void StartTheClock(){
+    t1 = high_resolution_clock::now();
+}
+
+double StopTheClock() {
+    t2 = high_resolution_clock::now();
+    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+    return time_span.count();
+}
+
+igraph_t generateGeometricGraph(igraph_integer_t n, igraph_real_t radius)
+{
+    igraph_t G_graph;
+    igraph_bool_t connected;
+
+    // generate a connected random graph using the geometric model
+    igraph_grg_game(&G_graph, n, radius, 0, nullptr, nullptr);
+
+    igraph_is_connected(&G_graph, &connected, IGRAPH_WEAK);
+    while(!connected){
+        igraph_destroy(&G_graph);
+        igraph_grg_game(&G_graph, n, radius, 0, nullptr, nullptr);
+
+        igraph_is_connected(&G_graph, &connected, IGRAPH_WEAK);
+    }
+
+    return G_graph;
+}
+
+igraph_t generateBarabasiAlbertGraph(igraph_integer_t n, igraph_real_t power, igraph_integer_t m, igraph_real_t A)
+{
+
+    // n = The number of vertices in the graph
+    // power = Power of the preferential attachment. The probability that a vertex is cited is proportional to d^power+A, where d is its degree, power and A are given by arguments. In the classic preferential attachment model power=1
+    // m = number of outgoing edges generated for each vertex
+    // A = The probability that a vertex is cited is proportional to d^power+A, where d is its degree, power and A are given by arguments
+
+    igraph_t BA_graph;
+    igraph_bool_t connected;
+
+    // generate a connected random graph using the Barabasi-Albert model
+    igraph_barabasi_game(/* graph=    */ &BA_graph,
+            /* n=        */ n,
+            /* power=    */ power,
+            /* m=        */ m,
+            /* outseq=   */ 0,
+            /* outpref=  */ 0,
+            /* A=        */ A,
+            /* directed= */ IGRAPH_UNDIRECTED,
+            /* algo=     */ IGRAPH_BARABASI_PSUMTREE,
+            /* start_from= */ 0);
+
+
+    igraph_is_connected(&BA_graph, &connected, IGRAPH_WEAK);
+    while(!connected){
+        igraph_destroy(&BA_graph);
+        igraph_barabasi_game(/* graph=    */ &BA_graph,
+                /* n=        */ n,
+                /* power=    */ power,
+                /* m=        */ m,
+                /* outseq=   */ 0,
+                /* outpref=  */ 0,
+                /* A=        */ A,
+                /* directed= */ IGRAPH_UNDIRECTED,
+                /* algo=     */ IGRAPH_BARABASI_PSUMTREE,
+                /* start_from= */ 0);
+
+        igraph_is_connected(&BA_graph, &connected, IGRAPH_WEAK);
+    }
+
+    return BA_graph;
+}
+
+igraph_t generateErdosRenyiGraph(igraph_integer_t n, igraph_erdos_renyi_t type, igraph_real_t param)
+{
+    // n = The number of vertices in the graph
+    // type = IGRAPH_ERDOS_RENYI_GNM G(n,m) graph, m edges are selected uniformly randomly in a graph with n vertices.
+    //      = IGRAPH_ERDOS_RENYI_GNP G(n,p) graph, every possible edge is included in the graph with probability p
+
+    igraph_t ER_graph;
+    igraph_bool_t connected;
+
+    // generate a connected random graph using the Erdos-Renyi model
+    igraph_erdos_renyi_game(&ER_graph, type, n, param, IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
+
+    igraph_is_connected(&ER_graph, &connected, IGRAPH_WEAK);
+    while(!connected){
+        igraph_destroy(&ER_graph);
+        igraph_erdos_renyi_game(&ER_graph, type, n, param, IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
+
+        igraph_is_connected(&ER_graph, &connected, IGRAPH_WEAK);
+    }
+
+    return ER_graph;
+}
+
+igraph_t generateRegularGraph(igraph_integer_t n, igraph_integer_t k)
+{
+    // n = The number of vertices in the graph
+    // k = The degree of each vertex in an undirected graph. For undirected graphs, at least one of k and the number of vertices must be even.
+
+
+    igraph_t R_graph;
+    igraph_bool_t connected;
+
+    // generate a connected regular random graph
+    igraph_k_regular_game(&R_graph, n, k, IGRAPH_UNDIRECTED, 0);
+
+    igraph_is_connected(&R_graph, &connected, IGRAPH_WEAK);
+    while(!connected){
+        igraph_destroy(&R_graph);
+        igraph_k_regular_game(&R_graph, n, k, IGRAPH_UNDIRECTED, 0);
+
+        igraph_is_connected(&R_graph, &connected, IGRAPH_WEAK);
+    }
+
+    return R_graph;
+}
+
+igraph_t generateRandomGraph(int type, int n)
+{
+    igraph_t random_graph;
+
+    switch (type) {
+        case 1:
+            random_graph = generateGeometricGraph(n, sqrt(100.0/(float)n));
+            break;
+        case 2:
+            random_graph = generateBarabasiAlbertGraph(n, 1.0, 5, 1.0);
+            break;
+        case 3:
+            random_graph = generateErdosRenyiGraph(n, IGRAPH_ERDOS_RENYI_GNP, 10.0/(float)n);
+            // random_graph = generateErdosRenyiGraph(n, IGRAPH_ERDOS_RENYI_GNM, ceil(n^2/3));
+            break;
+        case 4:
+            random_graph = generateRegularGraph(n, n-1);
+            break;
+
+        default:
+            break;
+    }
+
+    return random_graph;
+
+}
+
+void printGraphType(int type)
+{
+
+    switch (type) {
+        case 1:
+            cout << "Geometric random graph\n";
+            break;
+        case 2:
+            cout << "Barabasi-Albert random graph\n";
+            break;
+        case 3:
+            cout << "Erdos-Renyi random graph\n";
+            break;
+        case 4:
+            cout << "Regular random graph\n";
+            break;
+
+        default:
+            break;
+    }
+
+}
